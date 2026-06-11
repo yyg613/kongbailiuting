9902e779b2680a5a9e3d66c7c3da5bc9cc8beb01
yyg613 <2872944084@qq.com>
2026-06-11 19:56:57 +0800

    feat: 配置 GitHub Actions 自动部署至 Pages

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import MomentsView from '../views/MomentsView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/kongbailiuting/',
   server: {
     host: true,
     port: 5173
